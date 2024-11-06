b913da47cc85126ce293f7e66cad70e4ff427bf2
stocksage-capstoneproject <capstoneproject0207@gmail.com>
2024-11-06 23:37:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ pyo.init_notebook_mode(connected=True)
 
 # Load the CSV file with an alternate encoding
 try:
-    ticker_data = pd.read_csv('D:\\tickers.csv', encoding='ISO-8859-1')  # or encoding='latin1'
+    ticker_data = pd.read_csv('tickers.csv', encoding='ISO-8859-1')  # or encoding='latin1'
     print("Loaded CSV file successfully.")
     
     # Drop rows where the 'Name' column (or the stock name column) has missing values
